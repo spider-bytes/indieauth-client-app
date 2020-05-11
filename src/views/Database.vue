a3f6e60a4d12d00eb6f9d40c85ec038d8f356880
arstulke <arne.stulken2000@gmail.com>
2020-05-11 19:50:01 +0200

    retrieving spider-bytes address

--- src/views/Database.vue
+++ src/views/Database.vue
@@ -2,23 +2,43 @@
   <div class="about">
     <h1>This is the database management</h1>
     <div class="row">
+      <div class="col-2">UserId:</div>
+      <div class="col-10">{{userId}}</div>
+    </div>
+    <div class="row">
       <div class="col-2">Token:</div>
       <div class="col-10">{{token}}</div>
+    </div>
+    <div class="row">
+      <div class="col-2">Spider-Bytes Address:</div>
+      <div class="col-10">{{spiderBytesAddress}}</div>
     </div>
   </div>
 </template>
 
 <script>
+import IndieAuth from 'indieauth-helper';
+import { getAuthOptions } from '@/constants';
+
 export default {
   name: 'Database',
   components: {},
   data: function () {
     return {
+      userId: this.userId,
       token: this.token,
+      spiderBytesAddress: this.spiderBytesAddress,
     }
   },
   async mounted () {
+    this.userId = sessionStorage.getItem('step1.userId');
     this.token = sessionStorage.getItem('step2.token');
+
+    const auth = new IndieAuth(getAuthOptions());
+    const rels = await auth.getRelsFromUrl(this.userId, ['spider-bytes']);
+    this.spiderBytesAddress = rels['spider-bytes'];
+
+    // await fetch();
   }
 }
 </script>
