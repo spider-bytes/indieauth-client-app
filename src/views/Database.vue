486f8e22740fd12e1323ade772eb612be213efc1
arstulke <arne.stulken2000@gmail.com>
2020-05-12 12:12:20 +0200

    fetching list of databases

--- src/views/Database.vue
+++ src/views/Database.vue
@@ -17,47 +17,75 @@
       <div class="col-2">Database Token:</div>
       <div class="col-10">{{databaseToken}}</div>
     </div>
+    <div class="row">
+      <div class="col-2">Database List:</div>
+      <div class="col-10">
+        <ul v-if="dbList && dbList.length > 0">
+          <li v-for="db of dbList" :key="db">
+            {{db}}
+          </li>
+        </ul>
+        <span v-if="!(dbList && dbList.length > 0)"> - </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import IndieAuth from 'indieauth-helper';
-import { getAuthOptions } from '@/constants';
+  import IndieAuth from 'indieauth-helper';
+  import { getAuthOptions } from '@/constants';
 
-export default {
-  name: 'Database',
-  components: {},
-  data: function () {
-    return {
-      userId: this.userId,
-      userToken: this.userToken,
-      spiderBytesAddress: this.spiderBytesAddress,
-      databaseToken: this.databaseToken,
-    }
-  },
-  async mounted () {
-    this.userId = sessionStorage.getItem('step1.userId');
-    this.userToken = sessionStorage.getItem('step2.token');
-
-    const auth = new IndieAuth(getAuthOptions());
-    const rels = await auth.getRelsFromUrl(this.userId, ['spider-bytes']);
-    this.spiderBytesAddress = rels['spider-bytes'].replace(/\/$/g, '');
-
-    const response = await fetch(this.spiderBytesAddress + '/database-session', {
-      method: 'POST',
-      headers: {
-        Authorization: 'Bearer ' + this.userToken,
-        'IndieAuth-UserId': this.userId,
+  export default {
+    name: 'Database',
+    components: {},
+    data: function () {
+      return {
+        userId: this.userId,
+        userToken: this.userToken,
+        spiderBytesAddress: this.spiderBytesAddress,
+        databaseToken: this.databaseToken,
+        dbList: this.dbList,
       }
-    });
-    const databaseSessionBody = await response.json();
-    this.databaseToken = databaseSessionBody.token;
+    },
+    methods: {
+      createUserTokenHeaders: function () {
+        return {
+          Authorization: 'Bearer ' + this.userToken,
+          'IndieAuth-UserId': this.userId,
+        };
+      },
+      createDatabaseTokenHeaders: function () {
+        return {
+          Authorization: 'Bearer ' + this.databaseToken,
+        };
+      },
+      fetchDatabaseList: async function() {
+        const dbListRes = await fetch(this.spiderBytesAddress + '/database', { headers: this.createDatabaseTokenHeaders() });
+        this.dbList = (await dbListRes.json())
+          .databases
+          .map((database) => database.databaseId);
+      },
+    },
+    async mounted() {
+      this.userId = sessionStorage.getItem('step1.userId');
+      this.userToken = sessionStorage.getItem('step2.token');
 
-    // TODO shortterm add list database
-    // TODO shortterm add create database
-    // TODO shortterm add delete database
+      const auth = new IndieAuth(getAuthOptions());
+      const rels = await auth.getRelsFromUrl(this.userId, ['spider-bytes']);
+      this.spiderBytesAddress = rels['spider-bytes'].replace(/\/$/g, '');
+
+      const dbSessionRes = await fetch(this.spiderBytesAddress + '/database-session', {
+        method: 'POST',
+        headers: this.createUserTokenHeaders(),
+      });
+      const databaseSessionBody = await dbSessionRes.json();
+      this.databaseToken = databaseSessionBody.token;
+
+      await this.fetchDatabaseList();
+      // TODO shortterm add create database
+      // TODO shortterm add delete database
+    }
   }
-}
 </script>
 <style>
   .about {
